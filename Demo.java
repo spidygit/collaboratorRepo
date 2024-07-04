679990da0baa1b2da4137809dd11584fce83a666
samba <sivayalamanchili650@gmail.com>
2024-07-04 07:28:45 -0500

    message

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args){
+        int j=1;
+        for(int i=1;i<5;i++){
+        System.out.println("Hello");
+        j++;
+        };
+        System.out.println(j);
+    };
+};
